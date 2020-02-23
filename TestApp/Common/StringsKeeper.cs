d9175ba68367c8942992da6c2ed3427a1ad11f53
Midnight Empire <midnightempire@yandex.ru>
2020-02-23 23:29:04 +0400

    7-segment digits set-like implementation:
     - .NET core console test application created for current research and develop (RnD);
     - 7-segment digits set-like imolementation model was made;
     - Corresponding common infrastructure was added.

---
+++ TestApp/Common/StringsKeeper.cs
@@ -0,0 +1,34 @@
+﻿namespace TestApp.Common
+{
+    /// <summary>
+    /// Common strings values keeping class
+    /// </summary>
+    internal static class StringsKeeper
+    {
+        /// <summary>
+        /// Exception messages values keeping class
+        /// </summary>
+        internal static class ExceptionMessage
+        {
+            /// <summary>
+            /// No solution found exception message compution property
+            /// </summary>
+            public static string NoSolution => "No solutions found";
+
+            /// <summary>
+            /// Observation sequence is missing/incorrect one exception message compution property
+            /// </summary>
+            public static string MissingObservationSequence => "The sequence isn't found";
+
+            /// <summary>
+            /// Observation sequence is empty one exception message compution property
+            /// </summary>
+            public static string EmptyObservationsSequence => "There isn't enough data";
+
+            /// <summary>
+            /// Observation sequence is already finished one exception message compution property
+            /// </summary>
+            public static string ObservationIsAlreadyFinished => "The red observation should be the last";
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TestApp.Model.Common.EnumerableSet;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allDigits = SevenSegmentDigitModel.AllDigits;
+
+            foreach (var digit in allDigits)
+            {
+                Console.WriteLine(digit);
+            }
+        }
+    }
+}

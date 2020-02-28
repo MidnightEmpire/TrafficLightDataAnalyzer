ebd63bf0b253b7e8b219b40597f8bedee6759b39
Midnight Empire <midnightempire@yandex.ru>
2020-02-28 12:02:35 +0400

    Clock face segments broke up transformer tests added

--- TrafficLightDataAnalyzer/Model/RangeTransformer/TwoDigitClockFaceBrokeUpTransformationModel.cs
+++ TrafficLightDataAnalyzer/Model/RangeTransformer/TwoDigitClockFaceBrokeUpTransformationModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using TrafficLightDataAnalyzer.Interface;
 using TrafficLightDataAnalyzer.Model.ClockFace.ValuePresenter;
@@ -23,6 +24,11 @@ namespace TrafficLightDataAnalyzer.Model.RangeTransformer
         /// <returns>Result/transformed range items collection reference value</returns>
         public IEnumerable<TwoDigitClockFaceObservationBinaryCodeValueModel> Transform(IEnumerable<TwoDigitClockFaceValueModel> source)
         {
+            if (source == null)
+            {
+                throw new ArgumentOutOfRangeException(nameof(source));
+            }
+
             return source.Select((sourceItem) => new TwoDigitClockFaceObservationBinaryCodeValueModel(
                 (byte) (sourceItem.HigherDigitData.BinaryCode & ~this._brokeUpBinaryCodesMasks.HigherDigitData & SevenSegmentDigitModel.BinaryCodePatternMask),
                 (byte) (sourceItem.LowerDigitData.BinaryCode & ~this._brokeUpBinaryCodesMasks.LowerDigitData & SevenSegmentDigitModel.BinaryCodePatternMask)
@@ -35,6 +41,11 @@ namespace TrafficLightDataAnalyzer.Model.RangeTransformer
         /// <param name="brokeUpBinaryCodesMasks">Broke-up binary codes masks reference value. Each gidit specify broken segments by "1" bit, working one by "0" bit</param>
         public TwoDigitClockFaceBrokeUpTransformationModel(TwoDigitClockFaceObservationBinaryCodeValueModel brokeUpBinaryCodesMasks)
         {
+            if (brokeUpBinaryCodesMasks == null)
+            {
+                throw new ArgumentOutOfRangeException(nameof(brokeUpBinaryCodesMasks));
+            }
+
             this._brokeUpBinaryCodesMasks = brokeUpBinaryCodesMasks;
         }
     }
